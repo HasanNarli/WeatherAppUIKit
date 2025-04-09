c371b11f125ce7b2ad565e44fade759494fa676d
Hasan Narlı <hasannarli8@gmail.com>
2025-04-09 21:07:59 +0300

    Initial Commit

---
+++ WeatherAppHaso/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  WeatherAppHaso
+//
+//  Created by arge-macbook-air on 9.04.2025.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
---
+++ WeatherAppHasoTests/WeatherAppHasoTests.swift
@@ -0,0 +1,17 @@
+//
+//  WeatherAppHasoTests.swift
+//  WeatherAppHasoTests
+//
+//  Created by arge-macbook-air on 9.04.2025.
+//
+
+import Testing
+@testable import WeatherAppHaso
+
+struct WeatherAppHasoTests {
+
+    @Test func example() async throws {
+        // Write your test here and use APIs like `#expect(...)` to check expected conditions.
+    }
+
+}
